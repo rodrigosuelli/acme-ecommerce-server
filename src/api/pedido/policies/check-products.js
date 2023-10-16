b393601a7e7a070271535a98dad0c7130180c06a
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-16 13:04:02 -0300

    Checar qtd

--- src/api/pedido/policies/check-products.js
+++ src/api/pedido/policies/check-products.js
@@ -1,7 +1,6 @@
-// @ts-nocheck
 const { ApplicationError } = require('@strapi/utils').errors;
 
-module.exports = (policyContext, config, { strapi }) => {
+module.exports = async (policyContext, config, { strapi }) => {
   if (!policyContext.request.body.data.user) {
     return false; // block
   }
@@ -26,6 +25,36 @@ module.exports = (policyContext, config, { strapi }) => {
       'Adding the same product multiple times is not allowed'
     );
   }
+
+  // Checar qtd pedida e se produto esta ativo
+  const productsIdArray = produtos.map((produto) => produto.produto);
+
+  const entries = await strapi.entityService.findMany('api::produto.produto', {
+    filters: {
+      id: {
+        $in: productsIdArray,
+      },
+    },
+    fields: ['id', 'qtd_estoque', 'ativo'],
+  });
+
+  entries.forEach((entry) => {
+    if (entry.ativo === false) {
+      throw new ApplicationError(
+        `Não foi possível adicionar o produto de id ${entry.id} pois ele não está 'ativo'`
+      );
+    }
+
+    const correspondingProduct = produtos.find(
+      (produto) => parseInt(produto.produto) === entry.id
+    );
+
+    if (entry.qtd_estoque < correspondingProduct.qtd) {
+      throw new ApplicationError(
+        `Não foi possível adicionar o produto de id ${entry.id} pois não há qtd em estoque suficiente para seu pedido`
+      );
+    }
+  });
 
   return true; // pass
 };
