5b0308182469f08f2238a1382395a1dc4603de4b
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-23 09:36:04 -0300

    test4

--- src/api/produto/controllers/produto.js
+++ src/api/produto/controllers/produto.js
@@ -8,39 +8,45 @@ const api = require('../../../services/api');
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController(
-  'api::produto.produto',
-  ({ env, strapi }) => ({
-    executeExternalApi: async (ctx, next) => {
-      try {
-        console.log(ctx.request);
-        const requestBody = ctx.request.body;
-        // const authHeader = ctx.request.header['Authorization'];
-        // const secret = authHeader && authHeader.split(' ')[1];
+module.exports = createCoreController('api::produto.produto', ({ strapi }) => ({
+  executeExternalApi: async (ctx, next) => {
+    try {
+      const requestBody = ctx.request.body;
+      const paramsString = ctx.request.url;
 
-        // if (requestBody.model !== 'produto') {
-        // throw new Error('Status 400: Model is not produto');
-        // }
+      const searchParams = new URLSearchParams(paramsString);
+      const secret = searchParams
+        .get('/api/revalidate-frontend?secret')
+        .replace(' ', '+');
 
-        // if (secret !== env('NEXT_REVALIDATION_SECRET')) {
-        // throw new Error('Status 401: Invalid secret');
-        // }
-
-        const response = await api.post(env('FRONTEND_BASE_URL'), requestBody, {
-          headers: { Authorization: `Bearer ${secret}` },
-        });
-
-        console.log(response.data);
-
-        ctx.body = 'ok';
-      } catch (error) {
-        ctx.body = error;
-        if (error?.response) {
-          console.log(error.response);
-        } else {
-          console.log(error);
-        }
+      if (requestBody.model !== 'produto') {
+        console.log('Status 400: Model is not produto');
+        return ctx.badRequest('Model is not produto');
       }
-    },
-  })
-);
+
+      if (secret !== process.env.NEXT_REVALIDATION_SECRET) {
+        console.log('Status 401: Invalid secret');
+        return ctx.unauthorized('Invalid secret');
+      }
+
+      const response = await api.post(
+        `${process.env.FRONTEND_BASE_URL}/api/revalidate`,
+        requestBody,
+        {
+          headers: { Authorization: `Bearer ${secret}` },
+        }
+      );
+
+      console.log(response.data);
+
+      ctx.body = 'ok';
+    } catch (error) {
+      ctx.body = error;
+      if (error?.response) {
+        console.log(error.response);
+      } else {
+        console.log(error);
+      }
+    }
+  },
+}));
